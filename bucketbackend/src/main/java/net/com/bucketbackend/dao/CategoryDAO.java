112e4f92aa892f338416f65fa6401926ed5a5402
vikashbissu007 <vikashkumarbissu@gmail.com>
2018-06-17 17:14:13 +0530

    till part 04!!

--- bucketbackend/src/main/java/net/com/bucketbackend/dao/CategoryDAO.java
+++ bucketbackend/src/main/java/net/com/bucketbackend/dao/CategoryDAO.java
@@ -6,10 +6,14 @@ import net.com.bucketbackend.dto.Category;
 
 public interface CategoryDAO {
 
-	boolean add(Category category);
+	Category get(int id);
 
 	List<Category> list();
 
-	Category get(int id);
+	boolean add(Category category);
+
+	boolean update(Category category);
+
+	boolean delete(Category category);
 
 }
---
+++ bucketbackend/src/main/java/net/com/bucketbackend/dao/ProductDAO.java
@@ -0,0 +1,26 @@
+package net.com.bucketbackend.dao;
+
+import java.util.List;
+
+import net.com.bucketbackend.dto.Product;
+
+public interface ProductDAO {
+
+	Product get(int productId);
+
+	List<Product> list();
+
+	boolean add(Product product);
+
+	boolean update(Product product);
+
+	boolean delete(Product product);
+
+	// business methods
+	List<Product> listActiveProducts();
+
+	List<Product> listActiveProductsByCategory(int categoryId);
+
+	List<Product> getLatestActiveProducts(int count);
+
+}
